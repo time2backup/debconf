89b5c490adb1f1c7e66a6f13cc7cf11a8c84f9a1
Jean Prunneaux <jean@prunneaux.com>
2019-04-11 23:51:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# time2backup deb-builder config
+
+Use this config to build deb package for time2backup.
+
+Get time2backup: https://github.com/time2backup/time2backup
+
+Get deb-builder tool: https://github.com/pruje/deb-builder
---
+++ postbuild.sh
@@ -0,0 +1,8 @@
+# Custom commands after build
+# executed in the context of the root system
+# e.g. sudo chmod 755 usr && sudo chmod 755 usr/src
+
+cd usr/src/time2backup && \
+sudo chmod -R 755 . && \
+sudo chmod 644 *.md config/* docs/* inc/* locales/* resources/* \
+               libbash/*.* libbash/inc/* libbash/locales/*
